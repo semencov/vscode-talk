c10c8258cf05d5972a8cf3d2beab5de1224a39a0
Yuri Sementsov <hello@smcv.dev>
2024-01-05 20:22:04 +0200

    Fix type errors and add comments to code

--- src/00-types/02-wide.ts
+++ src/00-types/02-wide.ts
@@ -3,3 +3,5 @@ function sum(a, b) {
 }
 
 let b = sum(2, 2);
+let c = sum(2, '2');
+let d = sum(true, false);
--- src/00-types/03-narrow.ts
+++ src/00-types/03-narrow.ts
@@ -4,10 +4,12 @@ function record(move) {
   moves.push(move);
 }
 
+// Correct usage
 record({ direction: 'left', steps: 4 });
 record({ direction: 'up', steps: 2 });
 record({ direction: 'right', steps: 3 });
 
+// Incorrect usage
 record({ direction: 'north-north-west', steps: 8 });
 record('up');
 record(true);
