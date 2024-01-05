d25fdc575f27358cc4d568705ad761e57b1688d3
Yuri Sementsov <hello@smcv.dev>
2024-01-05 20:19:31 +0200

    Update type annotations and add new records to the moves array

--- src/00-types/03-narrow.ts
+++ src/00-types/03-narrow.ts
@@ -1,5 +1,13 @@
-let moves = [];
+let moves: any[] = [];
 
 function record(move) {
   moves.push(move);
 }
+
+record({ direction: 'left', steps: 4 });
+record({ direction: 'up', steps: 2 });
+record({ direction: 'right', steps: 3 });
+
+record({ direction: 'north-north-west', steps: 8 });
+record('up');
+record(true);
